3d37781a447bb5423787f7effc57cd6276500cec
Leandro <llimas@outlook.com>
2018-05-03 20:31:47 -0300

    inserido comando de inserts após criar as tabelas.

--- migrations/create_table_users.sql
+++ migrations/create_table_users.sql
@@ -1,6 +1,8 @@
-create table users(
-    id int NOT NULL AUTO_INCREMENT,
-    name varchar(100) NOT NULL,
-    email varchar(100) NOT NULL,
-    PRIMARY KEY (id)
-)
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `users` VALUES (1,'Leandro','leandro@leandro.com.br'),(2,'Daniel','daniel@daniel.com.br'),(3,'Robson','robson@robson.com.br'),(4,'Jurandir','jurandir@jurandir.com.br');
