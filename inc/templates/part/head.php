8a1d65987060d16317c807c87782e6308a6ca705
Pedro Azeredo <pedro.azeredo93@gmail.com>
2023-10-07 09:08:24 -0300

    feat: creating partition responsible for the head of the templates

---
+++ inc/templates/part/head.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-BR" data-lt-installed="true">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta id="viewport" name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="copyright" content="© 2023 [%author%]" />
+    <meta name="robots" content="all" />
+    <meta name="robots" content="max-image-preview:standard" />
+    <meta name="revisit-after" content="7 days" />
+    <meta name="description" content="[%description%]">
+    <meta name="author" content="[%author%]">
+    <meta name="theme-color" />
+    <meta name="msapplication-navbutton-color" />
+    <meta name="msapplication-TileColor" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta name="msapplication-TileImage" content="[%icon%]">
+
+    <link href="[%icon%]" rel="[%icon%]">
+
+    <link rel="stylesheet" type="text/css" href="./app-assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="./app-assets/css/bootstrap-extended.min.css">
+    <link rel="stylesheet" type="text/css" href="./app-assets/css/app.css">
+
+    <link rel="stylesheet" type="text/css" href="./app-assets/css/components.css">
+
+    <link rel="icon" href="[%icon%]" sizes="32x32">
+    <link rel="apple-touch-icon" href="[%icon%]">
+
+    <title>[%title%]</title>
+    [%css%]
+</head>
