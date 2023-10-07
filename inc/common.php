043a1181f072a60e0b99b3c5088cf55ead358eda
Pedro Azeredo <pedro.azeredo93@gmail.com>
2023-10-07 09:30:24 -0300

    feat: adding function for creating slugs

--- inc/common.php
+++ inc/common.php
@@ -15,6 +15,7 @@ session_start();
  */
 require_once("connection.php");	// connection sqlite
 require_once("config.php");	// configuration
+require_once("class.form.php"); // class.form
 
 date_default_timezone_set('America/Sao_Paulo');
 
@@ -55,9 +56,30 @@ function getDbValue($sql)
 // funcao para verificar permissao de arquivo
 function checkAccess()
 {
-	$access = getDbValue("SELECT id FROM cad_usuarios WHERE uniqid = '" . getSession('SYSGER') . "'");
+	$access = getDbValue("SELECT id FROM users WHERE id = '" . getSession('SYSGER') . "'");
 	if (empty($access)) {
 		setSession("SYSGER", "");
 		redirect('login.php');
 	}
 }
+
+// funcao para criar slugs
+function slug($string)
+{
+	$baseCaracters = array(
+		'Š' => 'S', 'š' => 's', 'Ð' => 'Dj', '' => 'Z', '' => 'z', 'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A',
+		'Å' => 'A', 'Æ' => 'A', 'Ç' => 'C', 'È' => 'E', 'É' => 'E',  'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I',
+		'Ï' => 'I', 'Ñ' => 'N', 'Ń' => 'N', 'Ò' => 'O', 'Ó' => 'O',  'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O', 'Ø' => 'O', 'Ù' => 'U', 'Ú' => 'U',
+		'Û' => 'U', 'Ü' => 'U', 'Ý' => 'Y', 'Þ' => 'B', 'ß' => 'Ss', 'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a',
+		'å' => 'a', 'æ' => 'a', 'ç' => 'c', 'è' => 'e', 'é' => 'e',  'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i', 'î' => 'i',
+		'ï' => 'i', 'ð' => 'o', 'ñ' => 'n', 'ń' => 'n', 'ò' => 'o',  'ó' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ø' => 'o', 'ù' => 'u',
+		'ú' => 'u', 'û' => 'u', 'ü' => 'u', 'ý' => 'y', 'ý' => 'y',  'þ' => 'b', 'ÿ' => 'y', 'ƒ' => 'f',
+		'ă' => 'a', 'î' => 'i', 'â' => 'a', 'ș' => 's', 'ț' => 't',  'Ă' => 'A', 'Î' => 'I', 'Â' => 'A', 'Ș' => 'S', 'Ț' => 'T',
+	);
+	$string = strtr(trim($string), $baseCaracters);
+	$string = str_replace("-", "", $string);
+	$string = str_replace(" ", "_", $string);
+
+	return strtolower($string);
+}
+
