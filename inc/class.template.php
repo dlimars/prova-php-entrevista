7ada118f785f3ce52d76ede4e80536679a2e5d76
Pedro Azeredo <pedro.azeredo93@gmail.com>
2023-10-08 04:25:09 -0300

    feat: creating functions to display topbar and sidebar

--- inc/class.template.php
+++ inc/class.template.php
@@ -121,6 +121,7 @@ class Template
         $outHtml = $this->__replace($outHtml,               "[%js%]",                $this->getJs);
         $outHtml = $this->__replace($outHtml,               "[%sweetalert%]",        getAlert());
         $outHtml = $this->__replace($outHtml,               "[%include_sidebar%]",   $this->getSidebar());
+        $outHtml = $this->__replace($outHtml,               "[%include_topbar%]",    $this->getTopbar());
 
         echo $outHtml;
     }
@@ -158,6 +159,47 @@ class Template
                     </ul>
                 </div>
             ";
+        return $outHtml;
+    }
+
+    /**
+     * gettopbar
+     * Responsável por criar o topbar
+     *
+     * @return void
+     */
+    private function getTopbar()
+    {
+        $outHtml = "
+            <nav class='header-navbar navbar navbar-expand-lg align-items-center floating-nav navbar-light navbar-shadow' data-aos='fade-down' data-aos-anchor-placement='top-bottom' data-aos-duration='800'>
+                <div class='navbar-container d-flex content'>
+                    <div class='bookmark-wrapper d-flex align-items-center'>
+                        <ul class='nav navbar-nav d-xl-none'>
+                            <li class='nav-item'><a class='nav-link menu-toggle' href='#'><i class='ficon' data-feather='menu'></i></a></li>
+                        </ul>
+                        <ul class='nav navbar-nav'>
+                        <!--<li class='nav-item d-none d-lg-block'><a class='nav-link nav-link-style'><i class='ficon' data-feather='moon'></i></a></li>-->
+                        </ul>
+                    </div>
+                    <ul class='nav navbar-nav align-items-center ms-auto'>
+                        <li class='nav-item dropdown dropdown-user'>
+                            <a class='nav-link dropdown-toggle dropdown-user-link' id='dropdown-user' href='#' data-bs-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+                                <div class='user-nav'>
+                                    <span class='user-name fw-bolder'>Pedro</span>
+                                    
+                                </div>
+                            </a>
+                            <div class='dropdown-menu dropdown-menu-end' aria-labelledby='dropdown-user'>
+                                <!-- <a class='dropdown-item' href='#'><i class='me-50' data-feather='user'></i> Perfil</a> -->
+                                <!-- <div class='dropdown-divider'></div> -->
+                                <a class='dropdown-item' href='loginSair.php'><i class='me-50' data-feather='log-out'></i> Sair</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            ";
+
         return $outHtml;
     }
 
