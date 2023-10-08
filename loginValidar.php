0977e26b576768fe1332656f0c58d1a31186198b
Pedro Azeredo <pedro.azeredo93@gmail.com>
2023-10-08 02:30:46 -0300

    feat: changing user query

--- loginValidar.php
+++ loginValidar.php
@@ -1,21 +1,26 @@
 <?php
-require_once("./inc/common.php");
 
+require_once("./inc/common.php");
 
 $email       = getParam("email");
 $password    = md5(getParam("password"));
 
-$user = getDbValue("SELECT COUNT(*) FROM users WHERE email = '" . $email . "' AND password = '" . $password . "' LIMIT 1");
-$user_id = getDbValue("SELECT id FROM users WHERE email = '" . $email . "' AND password = '" . $password . "' LIMIT 1");
+$query = new sqlQuery();
+$query->addTable("users");
+$query->addcolumn("id");
+$query->addWhere("email", "=", "'" . $email . "'");
+$query->addWhere("password", "=", "'" . $password . "'");
+$query->setLimit(1);
 
 try {
-
-    if ($user == 0) {
+    if ($conn->query($query->getSQL()) && getDbValue($query->getCount()) != 0) {
+        foreach ($conn->query($query->getSQL()) as $row) {
+            setSession('SYSGER', $row['id']);
+            redirect("index.php");
+        }
+    } else {
         throw new Exception("Erro ao logar, tente novamente");
     }
-
-    setSession('SYSGER', $user_id);
-    redirect("index.php");
 } catch (Exception $e) {
     setAlert($e->getMessage(), "error");
     redirect("login.php");
