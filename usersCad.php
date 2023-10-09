b952b36f84c0091557ac192180d0fe4ae30a4e83
Pedro Azeredo <pedro.azeredo93@gmail.com>
2023-10-09 16:12:48 -0300

    feat: creating crud for users

--- usersCad.php
+++ usersCad.php
@@ -24,10 +24,41 @@ $form = new Form("usersCadSave.php");
 $form->addField(hiddenField($user_id, "user_id"));
 $form->addField(textField("Nome", $f_nome, NUll, true));
 $form->addField(emailField("E-mail", $f_email, NUll, true, "^(?=.{1,256})(?=.{1,64}@)[^\s@]+@[^\s@]+\.[^\s@]{2,}$", NULL, "text-lowercase"));
-$form->addField(passField("Senha"));
-$form->addField(passField("Confirmar Senha"));
+if (!empty($user_id)) {
+    $form->addField('<button type="button" class="btn btn-outline-primary my-1 d-block" data-bs-toggle="modal" data-bs-target="#changePass">Alterar Senha</button>');
+} else {
+    $form->addField(passField("Senha", NULL, NULL, true, true));
+    $form->addField(passField("Confirmar Senha", NULL, NULL, true));
+}
 $form->addField(submitBtn("Salvar"));
+
+if (!empty($user_id)) {
+    $changePass = new Form("usersCadSave.php");
+    $changePass->addField(hiddenField($user_id, "user_id", "usuario_id"));
+    $changePass->addField(passField("Senha", NULL, NULL, true, true));
+    $changePass->addField(passField("Confirmar Senha", NULL, NULL, true));
+    $changePass->addField(submitBtn("Salvar"));
+
+    $modal = '
+    <div class="modal fade" id="changePass" tabindex="-1" aria-labelledby="changePassLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-md">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="changePassLabel">Digite sua nova senha!</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ' . $changePass->writeHtml() . '
+                </div>
+            </div>
+        </div>
+    </div>
+    ';
+}
 
 $template = new Template("Cadastro de Usuários");
 $template->addContent($form->writeHtml(), true);
+if (!empty($user_id)) {
+    $template->addContent($modal);
+}
 $template->writeHtml();
--- usersCadSave.php
+++ usersCadSave.php
@@ -1,114 +1,114 @@
-
 <?php
+
 require_once("./inc/common.php");
-checkAccess("usuariosList");
-
-$cad_usuarios_id        = getParam("cad_usuarios_id");
-$f_nome                 = getParam("f_nome");
-$f_email                = strtolower(getParam("f_email"));
-$f_senha                = getParam("f_senha");
-$f_confirmar_senha      = getParam("f_confirmar_senha");
-$f_ativo                = getParam("f_ativo");
-
-if ($f_ativo == "on") {
-    $f_ativo = "true";
-} else {
-    $f_ativo = "false";
-}
-
-validar_email($f_email);
-
-$dados = array(
-    "nome"            => $f_nome,
-    "email"           => $f_email,
-    "status"          => $f_ativo
-);
-
-if (!empty($f_senha) && !empty($f_confirmar_senha)) {
-    $dados["senha"] = validar_senha($f_senha, $f_confirmar_senha);
-}
+checkAccess("users");
 
 $e = getParam("e", true);
-$cad_usuario_id_delete = $e["cad_usuario_id_delete"];
-if ($cad_usuario_id_delete) {
-    $dados["id"] = $cad_usuario_id_delete;
-    $sql_delete = "DELETE FROM cad_usuarios WHERE id = :id";
+$user_id = $e["user_id"];
+$user_id_delete = $e["user_id_delete"];
 
-    try {
-        $conn->prepare($sql_delete)->execute(array("id" => $cad_usuario_id_delete));
+try {
+    if (!empty($user_id_delete)) {
+        $dados = array(
+            "id"           => getDbValue("SELECT id FROM users WHERE id =" . $user_id_delete),
+        );
+
+        $sql_delete = "DELETE FROM users WHERE id = :id";
+        $stmt = $conn->prepare($sql_delete);
+        $stmt->execute(['id' => $user_id_delete]);
         $actionText = "Exclusão efetuada com sucesso";
         $tipo = 'success';
-    } catch (PDOException $e) {
-        $actionText = "Erro ao excluir";
-        $tipo = 'error';
-    }
-} else {
-    if (!empty($cad_usuarios_id)) {
-
-        $dados["id"] = $cad_usuarios_id;
-
-        $sql_update = "
-		UPDATE cad_usuarios SET
-            nome = :nome,
-			email = :email,
-			status = :status,
-            dt_update = NOW()
-		";
-
-
-        if ($f_ativo == "0") {
-            $sql_update .= ", dt_delete = NOW()";
-        }
-
-        if (!empty($f_senha)) {
-            $sql_update .= ", senha = :senha,";
-        }
-
-        $sql_update .= "WHERE id = :id";
-
-        try {
-            $conn->prepare($sql_update)->execute($dados);
-            $lastInsertId = $cad_usuarios_id;
-            $actionText = "Alteração efetuada com sucesso";
-            $tipo = 'success';
-        } catch (PDOException $e) {
-            $actionText = "Erro ao alterar";
-            $tipo = 'error';
-        }
     } else {
-        $dados["uniqid"] = uniqIdNew();
+        $user_id                = getParam("user_id");
+        $f_name                 = getParam("f_nome");
+        $f_email                = strtolower(getParam("f_email"));
+        $f_password             = getParam("f_senha");
+        $f_confirmar_password   = getParam("f_confirmar_senha");
 
-        $sql_insert = "
-        INSERT INTO cad_usuarios (
-            nome, 
-            senha, 
-            email,
-            uniqid,
-            status,
-            dt_create
-        ) 
-        VALUES (
-            :nome, 
-            :senha, 
-            :email,
-            :uniqid, 
-            :status,
-            CURRENT_TIMESTAMP
-        )
-        RETURNING id;        
-		";
+        $dados = array(
+            "name"           => $f_name,
+            "email"          => $f_email
+        );
 
-        try {
-            $conn->prepare($sql_insert)->execute($dados);
-            $lastInsertId = $conn->lastInsertId();
-            $actionText = "Cadastro efetuado com sucesso";
-            $tipo = 'success';
-        } catch (PDOException $e) {
-            $actionText = "Erro ao cadastrar";
-            $tipo = 'error';
+        validar_email($f_email);
+
+        if (!empty($f_password) && !empty($f_confirmar_password) && !empty($user_id)) {
+
+            $dados_pass = array(
+                "id"             => $user_id,
+                "password"       => validar_senha($f_password, $f_confirmar_password),
+            );
+
+            $sql_update_pass = "UPDATE users SET password = :password WHERE id = :id";
+
+            $stmt = $conn->prepare($sql_update_pass);
+            $stmt->execute($dados_pass);
+            $actionText = "Alteração efetuada com sucesso";
+        } else {
+            if (!empty($user_id)) {
+                $dados["id"] = $user_id;
+
+                $sql_update = "
+				UPDATE users SET
+					name = :name,
+					email = :email
+                WHERE id = :id
+				";
+
+                $stmt = $conn->prepare($sql_update);
+                $stmt->execute($dados);
+                $lastInsertId = $user_id;
+                $actionText = "Alteração efetuada com sucesso";
+            } else {
+
+                if (!empty($f_password) && !empty($f_confirmar_password)) {
+                    $dados["password"] = validar_senha($f_password, $f_confirmar_password);
+                }
+
+                $sql_insert = "
+				INSERT INTO users (
+					name,
+					email,
+					password
+                ) VALUES (
+                    :name,
+                    :email,
+                    :password
+                )
+				";
+
+                $stmt = $conn->prepare($sql_insert);
+                $stmt->execute($dados);
+                $lastInsertId = $conn->lastInsertId();
+                $actionText = "Cadastro efetuado com sucesso";
+            }
         }
+        $tipo = 'success';
     }
+} catch (PDOException $e) {
+    if (!empty($user_id_delete)) {
+        $actionText = "Erro ao excluir";
+    } else if (!empty($user_id)) {
+        $actionText = "Erro ao alterar";
+    } else {
+        $actionText = "Erro ao cadastrar";
+    }
+
+    $extend = "text: 'Desculpe, ocorreu um erro";
+    if (!empty($user_id) || !empty($user_id_delete)) {
+        $extend .= " na ";
+        if (!empty($user_id)) {
+            $extend .= "alteração";
+        } else {
+            $extend .= "exclusão";
+        }
+        $extend .= ".";
+    } else {
+        $extend .= " no cadastro.";
+    }
+    $extend .= " Por favor, verifique os campos obrigatórios e/ou os dados inseridos. É possível que alguns dados já tenham sido utilizados.'";
+    $tipo = 'error';
 }
 
-setAlert($actionText, $tipo);
-redirect("usuariosList.php");
+setAlert($actionText, $tipo, $extend);
+redirect("users.php");
